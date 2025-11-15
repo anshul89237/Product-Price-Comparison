8248ed59c52068990944ad9472267a0c7f90ff44
Anshul Kumar <anshulkumar89237727@gmail.com>
2025-11-15 21:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask
 from flask_cors import CORS
-from flask import send_from_directory
+import json
 
-app = Flask("Product deal check")
+app = Flask("List of products")
 CORS(app)
 
-@app.route("/")
-def getHomePage():
-  return send_from_directory('html', "index.html")
+@app.route("/products")
+def getProductsList():
+  products = []
+  with open("products.json", "r") as prodfile:
+    data = json.load(prodfile)
+    for product in data["products"]:
+      products.append(product["product"])
+    return {"products":products}
+
+@app.route("/getdealers/<product>")
+def getDealers(product):
+  products = []
+  ret = False
+  with open("products.json", "r") as prodfile:
+    data = json.load(prodfile)
+    for productMeta in data["products"]:
+      if productMeta["product"] == product:
+        ret = True
+        return {"dealers":productMeta["Dealers"]}
+  if ret == False:
+    return {"message":"Could not find dealers for this product"}
 
 if __name__=="__main__":
-    app.run(port=5001,debug=True) 
+    app.run(debug=True) 
     # When no port is specified, starts at default port 5000
